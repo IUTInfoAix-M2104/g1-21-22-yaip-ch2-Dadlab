d5b8da5e7e5936177bee7724e9f0fd0f6882882a
David Gini <64867583+Dadlab@users.noreply.github.com>
2022-02-01 16:07:21 +0100

    Add files via upload

--- SumAverageRunningInt.java
+++ SumAverageRunningInt.java
@@ -4,22 +4,31 @@
 public class SumAverageRunningInt {   // Save as "SumAverageRunningInt.java"
    public static void main (String[] args) {
       // Define variables
-      int sum = 0;          // The accumulated sum, init to 0
-      double average;       // average in double
-      final int LOWERBOUND = 1;
-      final int UPPERBOUND = 100;
+      int sumOdd = 0;	  // The accumulated sum, init to 0
+      int sumEven = 0;
+	  int count = 0;
+	  double averageOdd;	  // average in double
+      double averageEven;
+	  final int LOWERBOUND = 111;
+      final int UPPERBOUND = 8899;
+	  int number = LOWERBOUND;
+	  
+      do {
+		 if (number % 2 == 0) {
+			sumOdd += number*number;
+		 }
+		 else {
+			 sumEven += number*number;
+		 }
+         ++number;
+		 ++count;
+      } while (number <= UPPERBOUND);
 
-      // Use a for-loop to sum from lowerbound to upperbound
-      for (int number = LOWERBOUND; number <= UPPERBOUND; ++number) {
-            // The loop index variable number = 1, 2, 3, ..., 99, 100
-         sum += number;     // same as "sum = sum + number"
-      }
       // Compute average in double. Beware that int / int produces int!
-	  double doubleUpperbound = UPPERBOUND;
-	  double doubleSum = sum;
-      average = doubleSum / doubleUpperbound;
+      averageOdd = (double)sumOdd / (double)count;
+	  averageEven = (double)sumEven / (double)count; 
       // Print sum and average
-      System.out.println( "The sum is " + sum );
-	  System.out.println( "The average " + average );
+      System.out.println( "The sum of even numbers squared from " + LOWERBOUND + " to " + UPPERBOUND + " is " + sumEven );
+	  System.out.println( "The sum of odd numbers squared from " + LOWERBOUND + " to " + UPPERBOUND + " is " + sumOdd );
    }
 }
